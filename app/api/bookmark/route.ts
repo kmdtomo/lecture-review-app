10b7327289d9fedcbca226d4f43082116b97c388
kmdtomo <tomo.wsf11@gmail.com>
2024-11-20 17:27:50 +0900

    Initial commit

--- app/api/bookmark/route.ts
+++ app/api/bookmark/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import prisma from "@/app/lib/prisma/prisma";
 import { BookMark } from "@/app/type/type";
 import { NextResponse } from "next/server";
