89d73f25ccf651047f87826cb61d38f7b0816535
kmdtomo <tomo.wsf11@gmail.com>
2024-11-20 17:55:10 +0900

    Initial commit

--- app/api/category/[categoryId]/route.ts
+++ app/api/category/[categoryId]/route.ts
@@ -1,10 +1,10 @@
 import { supabase } from "@/app/lib/supabase/supabaseClient"
 import { NextResponse } from "next/server"
 
-export async function GET(request:Request,{params}:{params:{categoryId:string}}): Promise<NextResponse>{
-    const {categoryId} = params
+export async function GET(request:Request,{params}:{params:{category:string}}): Promise<NextResponse>{
+    const {category} = params
     try{
-        const {data:lecture,error} = await supabase.from("lecture").select("*").eq("category",categoryId)
+        const {data:lecture,error} = await supabase.from("lecture").select("*").eq("category",category)
         if(error)
             return NextResponse.json({message:"講義内容が取れません"})
         return NextResponse.json(lecture,{status:200})
