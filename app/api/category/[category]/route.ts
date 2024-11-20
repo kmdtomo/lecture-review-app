37aa1d15c35140e2604344339f8890abdec4f28a
kmdtomo <tomo.wsf11@gmail.com>
2024-11-20 18:10:34 +0900

    Initial commit

--- app/api/category/[category]/route.ts
+++ app/api/category/[category]/route.ts
@@ -4,14 +4,15 @@ import { NextResponse } from "next/server"
 export async function GET(request:Request,{params}:{params:{category:string}}){
     const {category} = params
     try{
-        const {data:lecture,error} = await supabase.from("lecture").select("*").eq("category",category)
-        if(error)
-            return NextResponse.json({message:"講義内容が取れません"})
-        return NextResponse.json(lecture,{status:200})
 
-    }catch(error){
-        return NextResponse.json({error,message:"エラーが発生しました"},{status:500})
+        const{data,error} = await supabase.from("lecture").select("*").eq("category",category)
+        if(error){
+            return NextResponse.json(error,{status:500})
+        }
+        return NextResponse.json(data,{status:200})
 
+    }catch(err){
+        return NextResponse.json(err,{status:500})
 
     }
-} 
+}
